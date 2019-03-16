f7f91e6d412bd5b742fe3bb2a2993091c8bef96a
lenileiro <lenileiro@gmail.com>
2019-03-16 16:11:13 +0300

    code refactor

--- run.py
+++ run.py
@@ -6,15 +6,17 @@ import grpc
 import hello_pb2
 import hello_pb2_grpc
 from credentials.server import Credentials
-from service_response.hello import Greeter
+from app.hello import Greeter
 
 def run():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    
+    #register service to server
     hello_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     
     server_credentials = Credentials.get_credentials()
     
-    server.add_secure_port('localhost:50052', server_credentials)
+    server.add_secure_port('localhost:50051', server_credentials)
     server.start()
     try:
         while True:
