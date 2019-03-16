f9010827e8808b6e0c8c8281672d86e79299048a
lenileiro <lenileiro@gmail.com>
2019-03-16 18:43:23 +0300

    added modified fetch

--- nodeclientexample/fetch.js
+++ nodeclientexample/fetch.js
@@ -2,18 +2,18 @@ let grpc = require("grpc");
 var grpc_promise = require('grpc-promise')
 var client = require('./credentials')
 
-function run() {
+function fetch(name) {
     const meta = new grpc.Metadata();
     meta.add('key', 'value');
 
     grpc_promise.promisifyAll(client, { metadata: meta, timeout: 1000 }); // timeout in milliseconds
-
-    client.SayHello()
-    .sendMessage({name: "sam"})
-    .then(res => {
-        console.log('Client:', res.message) // Client:Message Received
-    })
-    .catch(err => console.error(err))
+    
+    return client.SayHello()
+    .sendMessage({name: name})
 }
 
-run()
+
+fetch("sam").then(res => {
+    console.log('Client:', res.message) // Client:Message Received
+})
+.catch(err => console.error(err))
