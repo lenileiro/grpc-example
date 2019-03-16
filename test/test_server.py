efd1482b2c8c74e524bc213aa971bb630fd3ea16
lenileiro <lenileiro@gmail.com>
2019-03-16 15:02:19 +0300

    clean test file

--- test/test_server.py
+++ test/test_server.py
@@ -1,19 +1,7 @@
 import unittest
-from server import Server
 from .client import Client
 
-server = Server()
 class CommentsTestClass(unittest.TestCase):
 
-    def setUp(self):
-        #self.server=server
-        #self.server.serve()
-        self.res= Client.run("sam")
-
-    def tearDown(self):
-        self.res= Client.run("end")
-        #server.serve(1)
-        #Server.serve(0)
-
     def test_response(self):
-        self.assertEqual(self.res, 'Hello, sam!')
+        self.assertEqual(Client.run("sam"), 'Hello, sam!')
